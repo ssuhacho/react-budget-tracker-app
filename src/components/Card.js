6fcfb992d3904427eaa4f953127bd7d550a2fd36
ssuhacho <ssuhacho@gmail.com>
2022-05-26 16:15:32 -0500

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,16 @@
+import './Card.css';
+
+function Card(props){
+    //any value set on the class named props
+    //is added to this long string of class names
+    //which is then finally set on the div inside of the card.
+    const classes = 'card ' + props.className;
+
+    return(
+        <div className= {classes}>
+        {props.children}
+        </div>
+    );
+}
+
+export default Card;
