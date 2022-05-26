8295176075145b06b78f0c4dcf1c7622b199e367
ssuhacho <ssuhacho@gmail.com>
2022-05-26 16:37:33 -0500

    second commit

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import './ExpenseDate.css';
 
-function ExpenseItem(props){
+const ExpenseItem = (props) =>{
     const month = props.date.toLocaleString('en-US', { month: 'long' });
     const day = props.date.toLocaleString('en-US', { day: '2-digit' });
     const year = props.date.getFullYear();
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import './Card.css';
 
-function Card(props){
+const Card = (props) =>{
     //any value set on the class named props
     //is added to this long string of class names
     //which is then finally set on the div inside of the card.
