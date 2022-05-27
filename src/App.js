10d2be7726bbe49920dffd4c4024816d7dd5b333
ssuhacho <ssuhacho@gmail.com>
2022-05-27 14:03:47 -0500

    using stateful lists

--- src/App.js
+++ src/App.js
@@ -1,43 +1,60 @@
-import React from 'react';
+import React, { useState } from "react";
 import Expenses from "./components/Expenses/Expenses";
-import NewExpense from './components/NewExpense/NewExpense';
+import NewExpense from "./components/NewExpense/NewExpense";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { 
+    id: "e2", 
+    title: "New TV", 
+    amount: 799.49, 
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
-  const addExpenseHandler = expense =>{
-    console.log('in App.js');
-    console.log (expense);
-  }
+  //expense data generated from NewExpense.js
+  const addExpenseHandler = (expense) => {
+    //use spread operator to pull out all the existing array elements
+    //and populate the rest of the new array with the existing elements.
+    //spread operator can be used to any array or object.
+
+    //when you updating a state depending on a previous state,
+    //should use state updating function form.
+    //passing a function as argument and the function will automatically
+    //receives the latest state snapshot.
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
-    {/* onAddExpense is a function that points to a function addExpenseHandler
+      {/* onAddExpense is a function that points to a function addExpenseHandler
     that's declared in this component */}
-      <NewExpense onAddExpense={addExpenseHandler}/>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -10,6 +10,7 @@ const NewExpense = (props) => {
             id: Math.random().toString()
         };
         props.onAddExpense(expenseData);
+        console.log(expenseData);
     }
 
     return <div className='new-expense'>
