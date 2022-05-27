10dc72dc139f8fad9260404dd2943a8093803933
ssuhacho <ssuhacho@gmail.com>
2022-05-27 14:52:21 -0500

    additing conditional return statement

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
 
 const Expenses = (props) => {
@@ -23,17 +23,9 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+        {/* shows a message when the filter has no found */}
+        <ExpensesList items={filteredExpenses}/>
 
-        {/* js array map */}
-        {/* transform the array to an array full of JSX items */}
-        {filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
       </Card>
     </div>
   );
